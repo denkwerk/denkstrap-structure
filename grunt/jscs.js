e406211f6002610a967d6a63a45a9606912227a7
Marius Büscher <marius.buescher@denkwerk.com>
2015-10-28 16:51:23 +0100

    Add jscs task

---
+++ grunt/jscs.js
@@ -0,0 +1,32 @@
+/**
+ * Grunt JSCS plugin
+ * https://github.com/jscs-dev/grunt-jscs
+ */
+module.exports = {
+    options: {
+            config: '.jscsrc',
+            fix: true
+        },
+    frontend: [
+        '<%= srcPath %>**/*.js'
+    ],
+    grunt: [
+        '<%= gruntPath %>**/*.js',
+        'Gruntfile.js'
+    ],
+    ci: {
+        options: {
+            fix: false,
+            force: true,
+            reporter: 'checkstyle',
+            reporterOutput: '<%= testResultPath %>jscs-lint-report.xml'
+        },
+        files: {
+            src: [
+                '<%= srcPath %>**/*.js',
+                '<%= gruntPath %>**/*.js',
+                'Gruntfile.js'
+            ]
+        }
+    }
+};
