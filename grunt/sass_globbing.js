126415e18080b28fa378857a8bc1c2cc10e7f222
Frank Uekermann <frank.uekermann@denkwerk.com>
2015-10-30 13:59:57 +0100

    [+TASK] added grid  + corrections

--- grunt/sass_globbing.js
+++ grunt/sass_globbing.js
@@ -5,10 +5,11 @@ module.exports = function (grunt, options) {
     return {
         dist: {
             files: {
+                '<%= srcPath %>components/sass/import/config.scss': '<%= srcPath %>components/sass/config/**/*.scss',
                 '<%= srcPath %>components/sass/import/variables.scss': '<%= srcPath %>components/sass/variables/**/*.scss',
                 '<%= srcPath %>components/sass/import/mixins.scss': '<%= srcPath %>components/sass/mixins/**/*.scss',
-                '<%= srcPath %>components/sass/import/vendor.scss': '<%= srcPath %>components/vendor/**/*.scss',
-                '<%= srcPath %>components/sass/import/site.scss': '<%= srcPath %>components/site/**/*.scss',
+                '<%= srcPath %>components/sass/import/vendor.scss': '<%= srcPath %>components/sass/vendor/**/*.scss',
+                '<%= srcPath %>components/sass/import/site.scss': '<%= srcPath %>components/sass/site/**/*.scss',
                 '<%= srcPath %>components/sass/import/patterns.scss': '<%= srcPath %>components/patterns/**/*.scss',
                 '<%= srcPath %>components/sass/import/modules.scss': '<%= srcPath %>components/modules/**/*.scss'
             },
