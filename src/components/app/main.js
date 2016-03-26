31c3094871ca85e59239ee3e9f511c4a51ba76a0
Marius Büscher <marius.buescher@denkwerk.com>
2016-03-26 16:34:25 +0100

    Removes IE8 compatible version of jQuery

--- src/components/app/main.js
+++ src/components/app/main.js
@@ -29,10 +29,7 @@
 
             // If it is not necessary to support IE8 its recommended to
             // use jQuery 2.X.X
-            //jquery:         '../vendor/jquery-2.1.3'
-
-            // For IE8 support use jquery 1.11.X
-            jquery:             'vendor/jquery-1.11.3'
+            jquery:         'vendor/jquery-2.1.3'
 
         },
 
