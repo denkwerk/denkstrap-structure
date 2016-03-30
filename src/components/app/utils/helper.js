6139a375c6e4f5adf16fcd4702a20b17edbd4e6d
Matthias Permien <matthias.permien@denkwerk.com>
2016-03-30 15:49:00 +0200

    Add once helper

--- src/components/app/utils/helper.js
+++ src/components/app/utils/helper.js
@@ -21,6 +21,28 @@
                     }
                 }
                 return baseObject;
+            },
+
+            /**
+             * The once helper
+             *
+             * This helper creates a function, which can be called just once
+             *
+             * @param  {Function}   fn      The function to be wrapped
+             * @param  {Object}     context The context in which the function should be called
+             * @return {Function}           The once wrapper
+             */
+            once: function( fn, context ) {
+                var result;
+
+                return function() {
+                    if ( fn ) {
+                        result = fn.apply( context || this, arguments );
+                        fn = null;
+                    }
+
+                    return result;
+                };
             }
         };
     } );
