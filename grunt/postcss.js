75cfcd46658b3781b6568ff5fdf1a440086910bb
Marius Büscher <marius.buescher@denkwerk.com>
2016-05-02 17:35:04 +0200

    Adds grunt-postcss with autoprefixer, closes #8

---
+++ grunt/postcss.js
@@ -0,0 +1,18 @@
+/**
+ * Grunt postcss plugin
+ * https://github.com/nDmitry/grunt-postcss
+ */
+module.exports = function( grunt, options ) {
+    return {
+        autoprefixer: {
+            options: {
+                processors: [
+                    require( 'autoprefixer' )( {
+                        browsers: [ 'last 2 versions', 'ie >= 8', 'Firefox >= 12', 'ios >= 7', 'android >= 4' ]
+                    } )
+                ]
+            },
+            src: '<%= distPath %>css/**/*.css'
+        }
+    };
+};
