eecea63ede6d7fecee87c52c458cb880bf3607a7
Marius Büscher <marius.buescher@denkwerk.com>
2016-08-23 21:34:27 +0200

    Fixes JSCS error

--- karma.conf.js
+++ karma.conf.js
@@ -59,7 +59,7 @@ module.exports = function( config ) {
                 { type: 'html', subdir: 'report-html' },
                 { type: 'lcov', subdir: 'report-lcov' },
                 { type: 'cobertura', subdir: '.', file: 'cobertura.xml' },
-                { type: 'teamcity', subdir: '.', file: 'teamcity.txt' },
+                { type: 'teamcity', subdir: '.', file: 'teamcity.txt' }
             ]
         }
     } );
