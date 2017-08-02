aefd48a985b907bf102a82905a83f0b01143d047
Jessica Lazarus <jessica.lazarus@denkwerk.com>
2017-08-02 11:49:49 +0200

    new feature: responsive iframes!

--- src/styleguide/js/sg-custom-fullview.js
+++ src/styleguide/js/sg-custom-fullview.js
@@ -250,13 +250,14 @@ var SGB = window.SGB || {};
         /*
         ** init iframe resizer
         ** https://github.com/davidjbradshaw/iframe-resizer
+        ** http://davidjbradshaw.github.io/iframe-resizer/
         */
 
         var iframes = iFrameResize( {
             //hide timeout warnings
             warningTimeout: 0,
             // include pos: absolute elements and bigger elements coming in when interacting with sth
-            heightCalculationMethod: 'max',
+            heightCalculationMethod: 'min',
             // recalcilate the sticky headers
             initCallback: function() {
                 _recalculateStickies();
@@ -460,6 +461,57 @@ var SGB = window.SGB || {};
             }
 
         }
+
+        /*
+        ** Responsive iframe view
+        ** jLaz
+        */
+
+        var iframes = document.querySelectorAll( 'iframe' );
+
+        var changeIframeWidth = function( event ) {
+            var newWidth = event.currentTarget.getAttribute( 'data-width' ),
+                navigationContainer = document.querySelector( '.sg-navigation-container' ),
+                buttonActiveClass = 'sg-btn-active',
+                responsiveToggleButton = document.querySelector(
+                    '.js-sg-btn-responsive-toggle.' + buttonActiveClass
+                );
+
+            Array.prototype.forEach.call( iframes, function( iframe ) {
+
+                // add this class to html for styling purposes
+                // we have a modal showing up from closing navigation till resizing iframe width & height
+                html.classList.add( 'sg-refreshing-modules-modal' );
+
+                // resize iframe width after navigation has closed
+                navigationContainer.addEventListener( 'transitionend', function( event ) {
+                    iframe.style.width = newWidth;
+                } );
+
+                // resize iframe height after iframe width is done
+                // also recalculate the sticky headers and remove the styling class
+                iframe.addEventListener( 'transitionend', function( event ) {
+                    iframe.iFrameResizer.resize();
+                    _recalculateStickies();
+                    html.classList.remove( 'sg-refreshing-modules-modal' );
+                } );
+
+                // set active class to clicked button
+
+                if ( responsiveToggleButton ) {
+                    responsiveToggleButton.classList.remove( buttonActiveClass );
+                    responsiveToggleButton.removeAttribute( 'disabled', 'disabled' );
+                }
+                event.currentTarget.classList.add( buttonActiveClass );
+                event.currentTarget.setAttribute( 'disabled', 'disabled' );
+            } );
+        };
+
+        Array.prototype.forEach.call( queryAll( '.js-sg-btn-responsive-toggle' ), function( el ) {
+            el.addEventListener( 'click', changeIframeWidth );
+            el.addEventListener( 'click', toggleNavigationContainer );
+            el.addEventListener( 'click', toggleNavigationZIndex );
+        } );
 
     }
 }( this, SGB ) );
